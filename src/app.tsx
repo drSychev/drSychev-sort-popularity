b76e0c2a369d308ebe96a74d7e44aff70f29dc71
Denis Sychev <sychev.d@market-soft.ru>
2023-09-23 00:39:15 +0300

    fix changes name menu item

--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,7 @@ import Track = Spicetify.Track;
 
 async function main() {
     let item = new Spicetify.ContextMenu.Item(
-        "Играть по рейтенгуwwwwwwww",
+        "Играть по рейтингу",
         addToQueue,
         isArtist,
         `<svg role="img" height="16" width="16" viewBox="0 0 20 20" fill="currentColor"><path d="M3.67 8.67h14V11h-14V8.67zm0-4.67h14v2.33h-14V4zm0 9.33H13v2.34H3.67v-2.34zm11.66 0v7l5.84-3.5-5.84-3.5z"></path></svg>`
